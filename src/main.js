2d06cd845e15c0c8b58d6344c4e3ac49eda1c7e8
Faziz <farisaziz22@gmail.com>
2021-08-13 10:57:50 +0700

    template vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
-import router from "./router/index"
-import store from "./store/index"
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+
+app.mount('#app')
